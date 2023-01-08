49b2236412f31a07c61855fe025e9100265d7981
Mrowr Purr <mrowr.purr@gmail.com>
2023-01-08 12:39:10 -0800

    That's better.

--- ExampleSpec.bat
+++ ExampleSpec.bat
@@ -15,11 +15,11 @@ goto :eof
     goto :eof
 
 :test_should_pass
-    %assert% 69 == 69 %is_true%
+    %assert_eq% 69 == 69
     goto :eof
 
 :test_should_fail
-    echo before fail
+    %assert_eq% 1 2
+    %assert_eq% Foo Bar %or_fail%
     %assert_eq% 69 420
-    echo after fail
     goto :eof
--- Spec.bat
+++ Spec.bat
@@ -4,6 +4,9 @@ setlocal EnableDelayedExpansion
 set SPEC_BAT_VERSION=0.1.0
 
 set fail=call %0 fail
+set assert_eq=call %0 assert_eq
+set "or_fail=& if exist %TEMP%\Spec_bat_test_failed goto :eof"
+
 if "%~1" == "fail" (
     echo.>%TEMP%\Spec_bat_test_failed
     set "_args=%*"
@@ -12,16 +15,12 @@ if "%~1" == "fail" (
     goto :eof
 )
 
-set assert_eq=call %0 assert_eq
 if "%~1" == "assert_eq" (
     if not "%~2" == "%~3" (
         %fail% expected "%~2" to equal "%~3"
     )
     goto :eof
 )
-
-set assert=if not
-set "is_true=( echo.>%TEMP%\Spec_bat_test_failed && goto :eof )"
 
 if not "%CURRENTLY_RUNNING_SPEC_FILE%" == "" goto :eof
 
@@ -40,13 +39,21 @@ goto :eof
 %=DO NOT REMOVE THIS LINE - This creates a variable for using newlines in PowerShell commands=%
 )
     set CURRENTLY_RUNNING_SPEC_FILE="%~1"
+    set SETUP=
+    set TEARDOWN=
+    for /f "usebackq delims=" %%i in (`
+        powershell -c "Select-String -Pattern ^:setup$ -Path '%~1' | %% { $_.Line }"
+    `) do set SETUP=:setup
+    for /f "usebackq delims=" %%i in (`
+        powershell -c "Select-String -Pattern ^:teardown$ -Path '%~1' | %% { $_.Line }"
+    `) do set TEARDOWN=:teardown
     for /f "usebackq delims=" %%i in (`
         powershell -c "Select-String -Pattern ^:test -Path '%~1' | %% { $_.Line }"
     `) do (
         call :reset_spec
         set SPEC_OUTPUT=
         for /f "usebackq delims=" %%x in (`
-            "%~1" :setup %%i :teardown
+            "%~1" %SETUP% %%i %TEARDOWN%
         `) do (
             set SPEC_OUTPUT=!SPEC_OUTPUT!^> %%x!\n!
         )
@@ -55,9 +62,6 @@ goto :eof
             echo ^!SPEC_OUTPUT!
         ) else (
             echo ^[PASS] %%i
-            @REM if "%VERBOSE%" == "true" (
-                echo ^!SPEC_OUTPUT!
-            @REM )
         )
     )
     set CURRENTLY_RUNNING_SPEC_FILE=
